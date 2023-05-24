aaf91c94b967bb818d2ba47ff2f3cd7f93887a05
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-05-23 23:33:36 -0300

    Initial Commit

---
+++ ex003/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos</title>
+</head>
+<body>
+    <h1>Teste de Tipos Primitivos</h1>
+    <?php
+        $num = 0x1A;
+        echo "o Valor da variável é $num";
+
+        $v = 300;
+        var_dump($v);
+    
+    ?>
+</body>
+</html>
