35893690040218b17121272e5da3035f1b962b83
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-05-30 15:10:37 -0300

    update cad.php

--- antecessorSucessor/cad.php
+++ antecessorSucessor/cad.php
@@ -12,16 +12,18 @@
     </header>
     <main>
         <h1> Resultado Final</h1>
-        <?php
+        <p>
+            <?php
 
-             $numero = $_GET["numero"];
-             echo "<p>O número escolhido foi <strong>$numero</strong>";
-             $ant = $numero - 1;
-             echo "<p>O número <i>antecessor</i> é <strong>$ant</strong>";
-             $suc = $numero + 1;
-             echo "<p>O número <i>sucessor</i> é <strong>$suc</strong>";
+                $numero = $_GET["numero"];
+                echo "<p>O número escolhido foi <strong>$numero</strong>";
+                $ant = $numero - 1;
+                echo "<p>O número <i>antecessor</i> é <strong>$ant</strong>";
+                $suc = $numero + 1;
+                echo "<p>O número <i>sucessor</i> é <strong>$suc</strong>";
 
-        ?>
+            ?>
+        </p>
         <p><a href="javascript:history.go(-1)">Voltar</a></p>
     </main>
 </body>
